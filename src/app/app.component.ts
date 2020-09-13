7aded4201cb28342cd4917fa9fda83a93df62a34
SoiBac1001 <congheit196@gmail.com>
2020-09-13 23:41:52 +0700

    create FE chat

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MessageService} from "../service/message.service";
+declare var SockJS;
+declare var Stomp;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Chat Room';
+  input;
+
+
+  constructor(private messageService : MessageService) {
+  }
+
+  sendMessage(){
+
+  }
+}
